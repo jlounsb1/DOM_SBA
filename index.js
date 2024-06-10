2ff6f9cab0783feb75d14835da81b43a3f8950c5
jlounsb1 <jefflounsbury@gmail.com>
2024-06-10 12:09:19 -0400

    created the basic structure of the page i want

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const header = document.querySelector('.header');
 const stats = document.querySelector('.stats');
 const walks = document.querySelector('.walks');
 const signUp = document.querySelector('.signup');
+const walkerInput = document.getElementById('walkerinput');
+const walkerName = walkerInput.elements['walkername'];
+const walkerDistance = walkerInput.elements['walkerdistance'];
+const walkHappiness = walkerInput.elements['happiness']
+const walkSubmit = walkerInput.elements['submit'];
+let radarStats = [];
 
+walkerInput.addEventListener('submit', handleClick);
 
-
+function handleClick(event) {
+    event.preventDefault();
+    event.alert('you clicked the button');
+}
