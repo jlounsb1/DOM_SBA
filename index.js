33500690d5a3db90e8486d826bb42cfb13936276
jlounsb1 <jefflounsbury@gmail.com>
2024-06-10 10:51:05 -0400

    added basic layout

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const app = document.getElementById('app');
+const header = document.querySelector('.header');
+const stats = document.querySelector('.stats');
+const walks = document.querySelector('.walks');
+const signUp = document.querySelector('.signup');
+
+
 
