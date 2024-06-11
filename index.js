0edb2bc59c9f5caadd539618bdd23eb645635c34
jlounsb1 <jefflounsbury@gmail.com>
2024-06-11 13:58:23 -0400

    cleaned up some lines and tweaked readme

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ const totalWalk = document.getElementById('totalwalk');
 const average = document.getElementById('average');
 const mostFrequent = document.getElementById('mostfrequent');
 const toStatsBtn = document.getElementById('topbtn');
-
 let radarStats = [];
 let walkNum =0;
 const theMoment = (new Date()).getTime()+1000000;
@@ -35,7 +34,6 @@ function handleClick(event) {
     div.classList.add('walk-item');
     div.innerHTML = `<h3>Walk: ${walkNum}</h3><p>Walked by: <span>${walkerName.value}</span></p><p>Date and Time: <span>${date.value}</span></p><p>Total Distance: <span>${walkerDistance.value}</span> Meters</p><p>Happiness Level: <span>${walkHappiness.value}</span></p><p>Notes:</p><p>${walkNotes.value}</p>`
     walksId.appendChild(div)
-  
     walkerInput.reset();
     window.alert("Thank you for Submitting your walk with Mr. Radar. I'm sure he appreciated it.");
     walks.classList.remove('hidden');
@@ -58,7 +56,6 @@ if(walkMoment>theMoment){
 radarStats.push(eachWalk)
 }
 
-
 function updateStats() {
 let noData = 0;
 let sum =0;
@@ -79,8 +76,7 @@ lastWalk.textContent = `${date.value}`
 mostFrequent.textContent = `${walkerName.value}`
 }
 
-
-function toStats(event) {
+function toStats() {
  lastWalk.parentNode.scrollIntoView({behavior: 'smooth'})
  //I could have just jumped to the top of the div, but I wanted to hit one of the requirements to use a parent-child relationship
  //found the scrollIntoView method to do this easily on stack Overflow, wanted to cite my source.
